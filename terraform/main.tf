10bd4e957e5ed4533b42da060db08e85e9d2437c
Gary Climacosa <gary.climacosa@aa.com>
2024-06-26 10:32:34 +0200

    update terraform to adjust to existing keypairs from previous tf testing in aws

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,16 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_key_pair" "existing" {
+  key_name = "terraform_aws_key"
+  filter {
+    name   = "key-name"
+    values = ["terraform_aws_key"]
+  }
+}
+
 resource "aws_key_pair" "deployer" {
+  count = length(data.aws_key_pair.existing.key_name) == 0 ? 1 : 0
   key_name   = "terraform_aws_key"
   public_key = file("~/.ssh/id_ed25519.pub")
 }
@@ -11,7 +20,7 @@ resource "aws_instance" "minikube" {
   ami               = "ami-04ffc9f7871904759" # Ubuntu Server 22.04 LTS (HVM)
   instance_type     = "t3.large"
   availability_zone = "us-east-1b"
-  key_name          = aws_key_pair.deployer.key_name
+  key_name          = coalesce(data.aws_key_pair.existing.key_name, "terraform_aws_key")
 
   root_block_device {
     volume_size = 50 # Size in GB
